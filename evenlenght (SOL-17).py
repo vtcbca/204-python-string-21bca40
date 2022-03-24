5c4053fcf0246bf5905b13757e9d2ca235fd7f2c
21bca40 <96609187+21bca40@users.noreply.github.com>
2022-03-24 11:15:26 +0530

    Add files via upload

---
+++ evenlenght (SOL-17).py
@@ -0,0 +1,20 @@
+#was to enter 5 string in a list and check and print string whoes lenght has even number of character without any string function.do this script UDF.
+l=[]
+def CreateList():
+    for i in range(5):
+        s=input("Enter any string:");
+        l.append(s)
+               
+def CountEven(l):
+    for i in l:
+            if l(i)%2==0:
+                count+=1
+
+    return(count)
+
+CreateList()
+print(l)
+
+
+
+
